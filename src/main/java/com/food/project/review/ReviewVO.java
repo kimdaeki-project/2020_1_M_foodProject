b1e46ef36fd46447b319167978c039dbd14cb14d
Gotaemin <gtm1213@naver.com>
2020-05-11 15:20:39 +0900

    ADD

--- src/main/java/com/food/project/review/ReviewVO.java
+++ src/main/java/com/food/project/review/ReviewVO.java
@@ -5,6 +5,13 @@ import com.food.project.board.BoardVO;
 public class ReviewVO extends BoardVO{
 
 	private int rating;
+	private long memberNum;
+	private long marketNum;
+	private long ref;
+	private long step;
+	private long depth;
+	
+	
 
 	public int getRating() {
 		return rating;
@@ -12,6 +19,46 @@ public class ReviewVO extends BoardVO{
 
 	public void setRating(int rating) {
 		this.rating = rating;
+	}
+
+	public long getMemberNum() {
+		return memberNum;
+	}
+
+	public void setMemberNum(long memberNum) {
+		this.memberNum = memberNum;
+	}
+
+	public long getMarketNum() {
+		return marketNum;
+	}
+
+	public void setMarketNum(long marketNum) {
+		this.marketNum = marketNum;
+	}
+
+	public long getRef() {
+		return ref;
+	}
+
+	public void setRef(long ref) {
+		this.ref = ref;
+	}
+
+	public long getStep() {
+		return step;
+	}
+
+	public void setStep(long step) {
+		this.step = step;
+	}
+
+	public long getDepth() {
+		return depth;
+	}
+
+	public void setDepth(long depth) {
+		this.depth = depth;
 	}
 	
 	
